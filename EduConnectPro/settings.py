236b67ba0ede89b86b1da64864b39428eaca78d0
Aidar <97720321+Aidar912@users.noreply.github.com>
2023-10-06 19:59:19 +0600

    create models project configuration

--- EduConnectPro/settings.py
+++ EduConnectPro/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'main',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -54,10 +55,8 @@ ROOT_URLCONF = 'EduConnectPro.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.jinja2.Jinja2'
-        ,
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,11 +69,24 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'EduConnectPro.wsgi.application'
 
+WSGI_APPLICATION = 'EduConnectPro.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'educonnect',
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 
 DATABASES = {
     'default': {
@@ -82,7 +94,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -102,16 +113,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 
@@ -119,8 +126,47 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
+}
+
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
+    'SLIDING_TOKEN_REFRESH_LIFETIME_LAMBDA': None,
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_REFRESH_SLIDING_CLAIM': 'sliding',
+    'SLIDING_TOKEN_REFRESH_SLIDING_REFRESH_CLAIM': 'sliding_refresh',
+    'ROTATE_REFRESH_TOKENS': False,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': None,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+
+}
--- main/models.py
+++ main/models.py
@@ -1,3 +1,217 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    login = models.CharField(max_length=50)
+    password = models.CharField(max_length=128)
+    fio = models.CharField(max_length=100, verbose_name="ФИО")
+    birthdate = models.DateField(verbose_name="Дата рождения")
+    phone_number = models.CharField(max_length=15 , verbose_name="Номер телефона")
+    address = models.CharField(max_length=200 , verbose_name="Адрес")
+    email = models.EmailField()
+    admission_year = models.PositiveIntegerField(verbose_name="Год поступления")
+    student_id = models.CharField(max_length=20,verbose_name="Номер ст билета")
+    photo = models.ImageField(upload_to='student_photos/', null=True, blank=True,verbose_name="Фото")
+    credits = models.PositiveIntegerField(verbose_name="Количество кредитов")
+    STUDENT_TYPES = (
+        ('budget', 'Бюджет'),
+        ('contract', 'Контракт'),
+    )
+    student_type = models.CharField(max_length=10, choices=STUDENT_TYPES , verbose_name="Статус студента")
+    account = models.OneToOneField('Account', on_delete=models.CASCADE, verbose_name="Счет",
+                                   related_name='student_account')
+    transcript = models.OneToOneField('Transcript', on_delete=models.SET_NULL, null=True, blank=True,verbose_name="Ведомость",related_name='transcripts_for_students')
+    group = models.ForeignKey('Group', on_delete=models.CASCADE,verbose_name="Группа")
+
+    def __str__(self):
+        return self.fio
+
+
+class Account(models.Model):
+    contract_number = models.CharField(max_length=20, verbose_name="Номер контракта")
+    date = models.DateField(verbose_name="Дата")
+    contract_term = models.DateField(verbose_name="Срок контракта")
+    payment_status = models.CharField(max_length=20, verbose_name="Статус оплаты")
+    payment_history = models.ForeignKey("PaymentHistory", on_delete=models.CASCADE, verbose_name="История оплаты")
+
+    def __str__(self):
+        return self.contract_number
+
+    def get_total_payment_amount(self):
+        total_payment_amount = PaymentHistory.objects.filter(account=self).aggregate(models.Sum('amount_paid'))['amount_paid__sum']
+        return total_payment_amount or 0
+
+
+
+class PaymentHistory(models.Model):
+    transaction_number = models.CharField(max_length=20, verbose_name="Номер транзакции")
+    student = models.ForeignKey('Student', on_delete=models.CASCADE, verbose_name="Студент")
+    payment_date = models.DateField(verbose_name="Дата оплаты")
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сумма оплаты")
+    PAYMENT_TYPES = (
+        ('online', 'Онлайн'),
+        ('cash', 'Наличными'),
+    )
+    payment_type = models.CharField(max_length=10, choices=PAYMENT_TYPES, verbose_name="Тип оплаты")
+
+    def __str__(self):
+        return self.transaction_number
+
+
+class Transcript(models.Model):
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE, verbose_name="Предмет")
+    grades = models.ManyToManyField('Grade', verbose_name="Оценки")
+    attendance = models.ManyToManyField('Attendance', verbose_name="Времена посещения")
+
+    def __str__(self):
+        return f"{self.student} - {self.subject}"
+
+
+class Attendance(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.CASCADE, verbose_name="Студент")
+    schedule = models.ForeignKey('Schedule', on_delete=models.CASCADE, verbose_name="Расписание")
+    ATTENDANCE_CHOICES = (
+        ('present', 'Был'),
+        ('absent', 'Не был'),
+        ('sick', 'Болел'),
+        ('excused', 'Уважительная причина'),
+    )
+    status = models.CharField(max_length=10, choices=ATTENDANCE_CHOICES, verbose_name="Статус")
+
+    def __str__(self):
+        return f"{self.student} - {self.schedule} - {self.status}"
+class Subject(models.Model):
+    name = models.CharField(max_length=100,verbose_name="Название")
+    subject_code = models.CharField(max_length=20,verbose_name="Номер предмета")
+    description = models.TextField(verbose_name="Описание")
+    credits = models.PositiveIntegerField(verbose_name="Количество кредитов")
+    teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE,verbose_name='Преподаватель')
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    fio = models.CharField(max_length=100, verbose_name="ФИО")
+    address = models.CharField(max_length=200, verbose_name="Адрес")
+    birthdate = models.DateField(verbose_name="Дата рождения")
+    phone_number = models.CharField(max_length=15, verbose_name="Телефон")
+    email = models.EmailField(verbose_name="Email")
+    subjects = models.ManyToManyField('Subject', related_name='teachers', verbose_name="Список предметов")
+    department = models.ForeignKey("Department", on_delete=models.CASCADE, verbose_name="Кафедра")
+
+    def __str__(self):
+        return self.fio
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название кафедры")
+    head_of_department = models.CharField(max_length=100, verbose_name="Заведующий кафедрой")
+    subjects = models.ManyToManyField('Subject', related_name='departments', verbose_name="Список предметов")
+    teachers = models.ManyToManyField('Teacher', related_name='departments', verbose_name="Список преподавателей")
+
+    def __str__(self):
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=100,verbose_name="Название")
+    year_created = models.PositiveIntegerField(verbose_name='Дата создания')
+    subjects = models.ManyToManyField('Subject', related_name='groups',verbose_name='Предметы')
+
+    def __str__(self):
+        return self.name
+
+
+class Schedule(models.Model):
+    WEEKDAYS = (
+        ('monday', 'Понедельник'),
+        ('tuesday', 'Вторник'),
+        ('wednesday', 'Среда'),
+        ('thursday', 'Четверг'),
+        ('friday', 'Пятница'),
+        ('saturday', 'Суббота'),
+        ('sunday', 'Воскресенье'),
+    )
+    weekday = models.CharField(max_length=10, choices=WEEKDAYS , verbose_name="День недели")
+    time = models.TimeField(verbose_name="Время")
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE,verbose_name="Предмет")
+    LESSON_TYPES = (
+        ('lecture', 'Лекция'),
+        ('practice', 'Практика'),
+    )
+    lesson_type = models.CharField(max_length=10, choices=LESSON_TYPES,verbose_name='Тип занятий')
+    teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE,verbose_name="Преподаватель")
+    group = models.ForeignKey('Group', on_delete=models.CASCADE,verbose_name="Группа")
+    classroom = models.CharField(max_length=20,verbose_name="Аудитория")
+
+    def __str__(self):
+        return f"{self.get_weekday_display()}, {self.time} - {self.subject} - {self.classroom}"
+
+
+class Grade(models.Model):
+    student = models.ForeignKey('Student', on_delete=models.CASCADE,verbose_name="Студент")
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE,verbose_name="Предмет")
+    grade = models.DecimalField(max_digits=20, decimal_places=1,verbose_name="Оценка")
+    date = models.DateField(verbose_name="Дата")
+    teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE,verbose_name="Преподаватель")
+
+    def __str__(self):
+        return f"{self.student} - {self.subject} - {self.grade}"
+
+
+class Library(models.Model):
+    book_title = models.CharField(max_length=100, verbose_name="Название книги")
+    author = models.CharField(max_length=100, verbose_name="Автор")
+    publication_year = models.PositiveIntegerField(verbose_name="Год издания")
+    students = models.ManyToManyField('Student', related_name='libraries', verbose_name="Студенты")
+    book_file = models.FileField(upload_to='library_books/', verbose_name="Файл для книги")
+
+    def __str__(self):
+        return self.book_title
+
+
+class Test(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название теста")
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE, verbose_name="Предмет")
+    start_date = models.DateField(verbose_name="Дата начала")
+    end_date = models.DateField(verbose_name="Дата окончания")
+    duration = models.PositiveIntegerField(verbose_name="Продолжительность (в минутах)")
+    group = models.ForeignKey('Group', on_delete=models.CASCADE, verbose_name="Группа")
+    grade = models.ForeignKey('Grade', on_delete=models.CASCADE, verbose_name="Оценка")
+    teacher = models.ForeignKey('Teacher', on_delete=models.CASCADE, verbose_name="Преподаватель")
+    attempts = models.PositiveIntegerField(verbose_name="Количество попыток")
+
+    def __str__(self):
+        return self.name
+
+
+class Module(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название модуля")
+    start_date = models.DateField(verbose_name="Дата начала")
+    end_date = models.DateField(verbose_name="Дата окончания")
+    description = models.TextField(verbose_name="Описание модуля")
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE, verbose_name="Предмет")
+    group = models.ForeignKey('Group', on_delete=models.CASCADE, verbose_name="Группа")
+
+    def __str__(self):
+        return self.name
+
+
+class Session(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название сессии")
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE, verbose_name="Предмет")
+    start_date = models.DateField(verbose_name="Дата начала")
+    end_date = models.DateField(verbose_name="Дата окончания")
+    subjects = models.ManyToManyField('Subject', related_name='sessions', verbose_name="Предметы")
+    students = models.ManyToManyField('Student', related_name='sessions', verbose_name="Студенты")
+    grades = models.ManyToManyField('Grade', verbose_name="Оценки")
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
